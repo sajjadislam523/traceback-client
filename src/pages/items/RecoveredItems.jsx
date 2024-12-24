d0a451982cd473e80fe13af06a236f79d2da7806
sajjadislam523 <sajjad.islam523@gmail.com>
2024-12-25 02:57:32 +0600

    Added layout change functionality

--- src/pages/items/RecoveredItems.jsx
+++ src/pages/items/RecoveredItems.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { CiGrid42, CiViewTable } from "react-icons/ci";
 import Loading from "../../components/Loading.jsx";
 import useAuth from "../../hooks/useAuth.jsx";
-
-import { Helmet } from "react-helmet-async";
+import useTheme from "../../hooks/useTheme.jsx";
 
 const RecoveredItems = () => {
     const { user } = useAuth();
     const [recoveredItems, setRecoveredItems] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [isTableLayout, setIsTableLayout] = useState(false);
+    const { theme } = useTheme();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -26,48 +29,72 @@ const RecoveredItems = () => {
         fetchData();
     }, [user.email]);
 
+    const toggleLayout = () => {
+        setIsTableLayout(!isTableLayout);
+    };
+
     return (
         <>
             <Helmet>
                 <title>Recovered Items - Traceback</title>
             </Helmet>
-            <div className="max-w-5xl mx-auto mt-8">
-                <h1 className="text-3xl font-bold text-center">
-                    My Recovered Items
-                </h1>
+            <div className={`p-12 ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-gray-900'}`}>
+                <h1 className="text-3xl font-bold text-center">My Recovered Items</h1>
+
+                <button
+                    onClick={toggleLayout}
+                    className="px-4 py-2 mt-4 text-gray-500 border rounded-md dark:text-gray-200"
+                >
+                    {isTableLayout ? <CiGrid42 /> : <CiViewTable />}
+                </button>
+
                 {loading ? (
                     <Loading />
                 ) : recoveredItems.length === 0 ? (
                     <p className="mt-4 text-center text-gray-500">No recovered items found.</p>
                 ) : (
-                    <div className="mt-8 overflow-x-auto">
-                        <table className="table table-zebra">
-                            <thead>
-                                <tr className="text-gray-700 bg-gray-200">
-                                    <th className="p-4 text-left border border-gray-300">Title</th>
-                                    <th className="p-4 text-left border border-gray-300">Category</th>
-                                    <th className="p-4 text-left border border-gray-300">Location</th>
-                                    <th className="p-4 text-left border border-gray-300">
-                                        Recovered Date
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                    <>
+                        {isTableLayout ? (
+                            <div className="mt-8 overflow-x-auto">
+                                <table className={`table table-zebra ${theme === 'dark' ? 'text-gray-200' : 'text-gray-700'} max-w-full`}>
+                                    <thead>
+                                        <tr className={`bg-gray-200 ${theme === 'dark' ? 'bg-gray-800' : ''}`}>
+                                            <th className="p-4 text-left border border-gray-300">Title</th>
+                                            <th className="p-4 text-left border border-gray-300">Category</th>
+                                            <th className="p-4 text-left border border-gray-300">Location</th>
+                                            <th className="p-4 text-left border border-gray-300">Recovered Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {recoveredItems.map((item) => (
+                                            <tr key={item._id} className={`${theme === 'dark' ? 'bg-gray-800 text-gray-50' : ''}`}>
+                                                <td className="p-4 border border-gray-300">{item.title}</td>
+                                                <td className="p-4 border border-gray-300">{item.category}</td>
+                                                <td className="p-4 border border-gray-300">{item.location}</td>
+                                                <td className="p-4 border border-gray-300">
+                                                    {new Date(item.dateRecovered).toLocaleDateString()}
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        ) : (
+                            <div className="grid grid-cols-1 gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3">
                                 {recoveredItems.map((item) => (
-                                    <tr
+                                    <div
                                         key={item._id}
+                                        className={`p-4 rounded-lg shadow-md ${theme === 'dark' ? 'bg-gray-900 text-gray-50' : 'bg-white'}`}
                                     >
-                                        <td className="p-4 border border-gray-300">{item.title}</td>
-                                        <td className="p-4 border border-gray-300">{item.category}</td>
-                                        <td className="p-4 border border-gray-300">{item.location}</td>
-                                        <td className="p-4 border border-gray-300">
-                                            {new Date(item.dateRecovered).toLocaleDateString()}
-                                        </td>
-                                    </tr>
+                                        <h2 className="text-xl font-bold">{item.title}</h2>
+                                        <p><strong>Category:</strong> {item.category}</p>
+                                        <p><strong>Location:</strong> {item.location}</p>
+                                        <p><strong>Recovered Date:</strong> {new Date(item.dateRecovered).toLocaleDateString()}</p>
+                                    </div>
                                 ))}
-                            </tbody>
-                        </table>
-                    </div>
+                            </div>
+                        )}
+                    </>
                 )}
             </div>
         </>
